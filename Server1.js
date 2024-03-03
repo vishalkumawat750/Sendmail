595b7a930b52aa536473554cf7f9d760bf2255d7
Vishal kumawat <60929278+vishalkumawat750@users.noreply.github.com>
2024-03-03 21:50:14 +0530

    Pdf in Mail

--- Server1.js
+++ Server1.js
@@ -3,6 +3,8 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const sharp = require('sharp');
+const { PDFDocument, rgb, StandardFonts  } = require('pdf-lib');
+const fs = require('fs');
 
 const app = express();
 
@@ -16,12 +18,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Handle POST request to /send-email
 app.post('/send-email', async (req, res) => {
   try {
-    const { name, email, message } = req.body;
+    const { name, email, message, subject } = req.body;
     console.log("api called!");
     
-    //EditSharpImage();
-    //EditGMImage();
+    var outputFile = 'output.pdf';
+    editTextInPDF('input.pdf', outputFile, name);
 
+    await sendEmail(email, name, message, subject, outputFile);
+    
+    console.log("Email sent successfully");
+    res.status(200).send('Email sent successfully');
+    
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).send('Error sending email');
+  }
+});
+
+async function sendEmail(email, name, message, subject, filename){
+  
     // Create Nodemailer transporter
     const transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -31,76 +46,45 @@ app.post('/send-email', async (req, res) => {
       }
     });
 
+    // Read the PDF file content
+    const pdfAttachment = await fs.promises.readFile(filename);
+
     // Send email using Nodemailer
     await transporter.sendMail({
       from: 'JigarKumawat236@gmail.com',
       to: email,
-      subject: 'Thank you from LaPhysics',
-      text: `Dear ${name},\n\nThank you for supporting us. We have received your message: ${message}\n\nBest regards,\nBhavans Physics Society. La Physics`,
+      subject: subject,
+      text: `Dear ${name},\n\n${message}`,
       attachments: [
         {
-          filename: 'ThankYouLetter.jpg', // Name of the attachment file
-          path: 'ThankYouLetter.png', // Path to the image file on your local filesystem
-          cid: 'image' // Content ID for referencing the image in the HTML content
+          filename: 'ThankYouLetter.pdf', // Name of the attachment file
+          content: pdfAttachment, // Content of the attachment (PDF file content)
+          contentType: 'application/pdf' // Content type of the attachment
         }
       ]
     });
 
-    res.status(200).send('Email sent successfully');
-  } catch (error) {
-    console.error('Error sending email:', error);
-    res.status(500).send('Error sending email');
-  }
-});
-
-function EditSharpImage(){
-  sharp('../thankyou/White.jpg')
-    .metadata()
-    .then(metadata => {
-      console.log("metadata : ", metadata);
-      const imageHeight = metadata.height;
-      const textPositionPercentage = 0.1; // 40% from the top
-      const textYCoordinate = imageHeight * textPositionPercentage;
-      console.log("textYCoordinate : ", textYCoordinate);
-      return sharp('../thankyou/White.jpg')
-      .composite([{
-        input: Buffer.from(`<svg><text x="10%" y="100%" font-family="Arial" font-size="30px" fill="black">Hello, world!</text></svg>`),
-        gravity: 'north' // Position the text at the top of the image
-      }])
-        .toFile('output.jpg', (err, info) => {
-          if (err) {
-            console.error('Error processing image:', err);
-          } else {
-            console.log('Image processed successfully:', info);
-          }
-        });
-    })
-    .catch(err => {
-      console.error('Error fetching image metadata:', err);
-    });
 }
 
+async function editTextInPDF(inputFilePath, outputFilePath, newText) {
+  const pdfDoc = await PDFDocument.load(fs.readFileSync(inputFilePath));
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+  const { width, height } = firstPage.getSize();
 
-function EditGMImage(){
-  const gm = require('gm').subClass({ imageMagick: true });
-
-const imagePath = 'White.jpg';
-const outputImagePath = 'output.jpg';
-
-// Add text to the image
-gm(imagePath)
-.font('Arial', 50)
-  .fill('black')
-  .drawText(0, 0, 'Hello, world!', 'NorthWest') // 'NorthWest' gravity
-  .write(outputImagePath, (err) => {
-    if (err) {
-      console.error('Error processing image:', err);
-    } else {
-      console.log('Image processed successfully');
-    }
+  // const fontBytes = await fs.promises.readFile('Oregano-Regular.ttf'); // Adjust the path to your font file
+  // const customFont = await pdfDoc.embedFont(fontBytes);
+  let font = await pdfDoc.embedFont(StandardFonts.TimesRomanBoldItalic);
+  firstPage.drawText(newText, {
+      x: 190,
+      y: height - 485,
+      size: 32,
+      font: font,
+      color: rgb(0,0,0),
   });
-
+  fs.writeFileSync(outputFilePath, await pdfDoc.save());
 }
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
